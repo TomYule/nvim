783b555259e2ceee31f2b59bdaa0e4cb954d82be
haiming <brave.yhm@gmail.com>
2024-05-11 14:34:42 +0800

    add:java支持

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+# 我的nvim Java开发配置如下
+电脑安装插件
+``` 
+https://github.com/BurntSushi/ripgrep
+https://www.nerdfonts.com/
+```
+
+Java插件支持
+``` 
+LazyExtras 按x安装dap.core
+
+https://github.com/nvim-java/nvim-java
+https://github.com/nvim-java/nvim-java/wiki/Lazyvim
+```
---
+++ lua/plugins/java/init.lua
@@ -0,0 +1,46 @@
+return {
+  "nvim-java/nvim-java",
+  dependencies = {
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-refactor",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
+      },
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        handlers = {
+          ["jdtls"] = function()
+            require("java").setup()
+          end,
+        },
+      },
+    },
+  },
+  opts = {
+    -- 添加这个部分
+    dap_config = {
+      java = {
+        {
+          type = "java",
+          request = "launch",
+          name = "Debug (Attach) - Remote",
+          hostName = "localhost",
+          port = 5005,
+        },
+      },
+    },
+  },
+}
